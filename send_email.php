4b7a2797ff699ab75277826cbc6a41cf3c0d56fe
Neveris-San <severin.prebois@gmail.com>
2024-10-04 22:38:32 +0200

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -8,7 +8,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $heures_sup = $_POST['heures_sup'];
     $commentaires = $_POST['commentaires'];
 
-    $to = "destinataire@example.com"; // Remplacez par l'adresse e-mail du destinataire
+    $to = "severin.prebois@gmail.com";
     $subject = "Formulaire de suivi quotidien - $date";
     $message = "
         Date : $date\n
@@ -20,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         Commentaires : $commentaires
     ";
 
-    $headers = "From: suivi-quotidien@example.com"; // Remplacez par l'adresse e-mail d'envoi
+    $headers = "From: severin.prebois@gmail.com";
 
     if (mail($to, $subject, $message, $headers)) {
         echo "Formulaire envoyé avec succès.";
